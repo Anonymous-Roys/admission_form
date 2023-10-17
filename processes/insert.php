82e0720518740259e5d5f7cc8898a0416d78fe7f
David Arhin <davidarhin2005@gmail.com>
2023-10-17 21:22:13 +0000

    backend stuff

--- processes/insert.php
+++ processes/insert.php
@@ -16,15 +16,15 @@ if(isset($_POST['submit'])){
 
 // ----------parent's Info ---------
 
-    $gender = $_POST['gender_c'];
-    $title = $_POST['title_c'];
-    $surname = $_POST['surname_c'];
-    $firstname = $_POST['firstname_c'];
-    $middlename = $_POST['middlename_c'];
-    $email = $_POST['email_c'];
-    $date_of_birth = $_POST['date_of_birth_c'];
-    $place_of_birth = $_POST['place_of_birth_c'];
-    $residential_address = $_POST['residential_address_c'];
+    $gender = $_POST['gender_p'];
+    $title = $_POST['title_p'];
+    $surname = $_POST['surname_p'];
+    $firstname = $_POST['firstname_p'];
+    $middlename = $_POST['middlename_p'];
+    $email = $_POST['email_p'];
+    $date_of_birth = $_POST['date_of_birth_p'];
+    $place_of_birth = $_POST['place_of_birth_p'];
+    $residential_address = $_POST['residential_address_p'];
     $house_number = $_POST['house_number_c'];
     
 // || empty($residential_address) || empty($email) || empty($house_number)
