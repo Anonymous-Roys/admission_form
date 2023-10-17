fbb3f8a09225484d88b55737178f816e9717ce72
David Arhin <davidarhin2005@gmail.com>
2023-10-17 21:19:33 +0000

    backend stuff

--- processes/insert.php
+++ processes/insert.php
@@ -2,35 +2,45 @@
 require_once('conn.php');
 
 if(isset($_POST['submit'])){
-    $gender = $_POST['gender'];
-    $title = $_POST['title'];
-    $surname = $_POST['surname'];
-    $firstname = $_POST['firstname'];
-    $middlename = $_POST['middlename'];
-    $email = $_POST['email'];
-    $date_of_birth = $_POST['date_of_birth'];
-    $place_of_birth = $_POST['place_of_birth'];
-    $residential_address = $_POST['residential_address'];
-    $house_number = $_POST['house_number'];
-    $secret_code = $_POST['secret_code'];
+    // --------children's info--------
+    $gender = $_POST['gender_c'];
+    $surname = $_POST['surname_c'];
+    $firstname = $_POST['firstname_c'];
+    $middlename = $_POST['middlename_c'];
+    $email = $_POST['email_c'];
+    $date_of_birth = $_POST['date_of_birth_c'];
+    $place_of_birth = $_POST['place_of_birth_c'];
+    $residential_address = $_POST['residential_address_c'];
+    $house_number = $_POST['house_number_c'];
+
+
+// ----------parent's Info ---------
+
+    $gender = $_POST['gender_c'];
+    $title = $_POST['title_c'];
+    $surname = $_POST['surname_c'];
+    $firstname = $_POST['firstname_c'];
+    $middlename = $_POST['middlename_c'];
+    $email = $_POST['email_c'];
+    $date_of_birth = $_POST['date_of_birth_c'];
+    $place_of_birth = $_POST['place_of_birth_c'];
+    $residential_address = $_POST['residential_address_c'];
+    $house_number = $_POST['house_number_c'];
     
-// || empty($residential_address) || empty($email) || empty($hashedhouse_number)
-    if (empty($gender) || empty($title) || empty($surname) || empty($firstname) || empty($middlename) || empty($email) || empty($date_of_birth) || empty($place_of_birth) || empty($residential_address)){
-        echo "Please fill in the blank spaces";
+// || empty($residential_address) || empty($email) || empty($house_number)
+
+
+    $query = "INSERT INTO student(gender, title, surname, firstname, middlename, email, date_of_birth, place_of_birth, residential_address, secret_code) 
+    VALUES ('$gender', '$title', '$surname',  '$firstname', '$middlename', '$email', '$date_of_birth', '$place_of_birth', '$residential_address', '$secret_code')";
+
+    $result = mysqli_query($conn, $query);
+
+    if($result){
+        header("Location: view.php");
     }else{
-
-        $query = "INSERT INTO student(gender, title, surname, firstname, middlename, email, date_of_birth, place_of_birth, residential_address, secret_code) 
-        VALUES ('$gender', '$title', '$surname',  '$firstname', '$middlename', '$email', '$date_of_birth', '$place_of_birth', '$residential_address', '$secret_code')";
-
-        $result = mysqli_query($conn, $query);
-
-        if($result){
-            header("Location: view.php");
-        }else{
-            echo "There is an error in your QUERY statement. Please try again";
-        }
-
+        echo "There is an error in your QUERY statement. Please try again";
     }
+
 }
 else{
     header("location: index.php");
